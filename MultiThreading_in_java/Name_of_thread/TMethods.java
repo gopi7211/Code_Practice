a98f88c3ee1d5d82fcc9cd58217e2e401b733fb7
GOPALA KRISHNA J <79375218+gopi7211@users.noreply.github.com>
2023-09-01 06:57:04 +0530

    Name of the Thread

---
+++ MultiThreading_in_java/Name_of_thread/TMethods.java
@@ -0,0 +1,9 @@
+package com.gopi7211.Name_of_thread;
+
+public class TMethods {
+    public static void main(String[] args) {
+        name_thred t1 = new name_thred();
+        new name_thred();
+        System.out.println("Name of Thread is"+t1.getName());
+    }
+}
---
+++ MultiThreading_in_java/Name_of_thread/name_thred.java
@@ -0,0 +1,7 @@
+package com.gopi7211.Name_of_thread;
+
+public class name_thred extends Thread{
+    public void run(){
+        System.out.println("------");
+    }
+}
