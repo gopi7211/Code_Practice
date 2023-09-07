2167360ad89b513ffcab1237da77154b35f022a9
GOPALA KRISHNA J <79375218+gopi7211@users.noreply.github.com>
2023-09-07 22:31:29 +0530

    Try block

---
+++ Exception handling/try_catch_block.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+		int results = 100;
+		
+		try{
+		    System.out.println(results/0);
+		    }
+		    catch(ArithmeticException e){
+		        System.out.println(e);
+		          }
+
+	}
+}   finally {
+		       System.out.println("Finally block always execute");
+		         }
+	}
+}
