de5ac4e5824241d53704e30e32ab5a03de9eb779
GOPALA KRISHNA J <79375218+gopi7211@users.noreply.github.com>
2023-09-14 00:09:08 +0530

    insert into

---
+++ MySQL/Basic Commands/Insert_into.sql
@@ -0,0 +1,9 @@
+create table nam(
+Firstname varchar(23),lastname varchar(23),age int
+)
+
+insert into Customer
+values('Archana','acchu',45),('Archana','acchu',45),('Subbu','Lakshmi',67),('gopi','krishna',21),('lachi','Lakshmi',21)
+
+select * from Customer;
+
