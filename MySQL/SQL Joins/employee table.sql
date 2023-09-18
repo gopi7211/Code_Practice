3df215f33d9737e93dfc5d47095bda411d189e14
GOPALA KRISHNA J <79375218+gopi7211@users.noreply.github.com>
2023-09-19 00:26:33 +0530

    Employee table

---
+++ MySQL/SQL Joins/employee table.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE EMPLOYEE (
+--    EID INT NOT NULL,
+--    EMPLOYEE_NAME VARCHAR (30) NOT NULL,
+--    SALES_MADE DECIMAL (20)
+-- );
+
+-- INSERT INTO EMPLOYEE VALUES
+-- (102, 'SARIKA', 4500),
+-- (100, 'ALEKHYA', 3623),
+-- (101, 'REVATHI', 1291),
+-- (103, 'VIVEK', 3426);
+
+select * from employee
