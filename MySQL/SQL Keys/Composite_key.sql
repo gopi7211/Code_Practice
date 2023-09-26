ae810c13a173f8ea257742334954ca49f798a81e
GOPALA KRISHNA J <79375218+gopi7211@users.noreply.github.com>
2023-09-26 23:02:35 +0530

    Composite_Key

---
+++ MySQL/SQL Keys/Composite_key.sql
@@ -0,0 +1,13 @@
+-- create TABLE animals(
+--    ID INT NOT NULL,
+--    NAME VARCHAR (20) NOT NULL,
+--    AGE INT NOT NULL,
+--    ADDRESS CHAR (25),
+--    SALARY DECIMAL (18, 2),       
+--    CONSTRAINT ck_customers 
+--    PRIMARY KEY (ID, NAME)
+-- );
+
+INSERT INTO animals VALUES 
+(1, 'Ramesh', 32, 'Ahmedabad', 2000.00 ),
+(2, 'Ramesh', 25, 'Delhi', 1500.00 );
