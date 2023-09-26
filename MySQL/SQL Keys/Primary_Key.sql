97ceb551e58cdc8a350bf8887ecfba8a1fedf3a5
GOPALA KRISHNA J <79375218+gopi7211@users.noreply.github.com>
2023-09-26 22:22:28 +0530

    Primary_key

---
+++ MySQL/SQL Keys/Primary_Key.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE relations (
+--    ID INT NOT NULL ,
+--    NAME VARCHAR(20) NOT NULL,
+--    AGE INT NOT NULL,
+--    ADDRESS CHAR (25),
+--    SALARY DECIMAL (18, 2)
+-- );
+
+INSERT INTO relations VALUES 
+-- (1, 'Ramesh', 32, 'Ahmedabad', 2000.00 ),
+-- (2, 'Khilan', 25, 'Delhi', 1500.00 ),
+(3, 'gopi', 25, 'Delhi', 1500.00 );
+
+
+-- ALTER TABLE relations add CONSTRAINT primary key (id);
+select *from relations
