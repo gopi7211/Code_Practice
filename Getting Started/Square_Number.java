71aaa68018e68dd6edc150f396c4cb70c2fc0817
GOPALA KRISHNA J <79375218+gopi7211@users.noreply.github.com>
2023-09-29 15:40:32 +0530

    Square Number

---
+++ Getting Started/Square_Number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	       Scanner sc = new Scanner(System.in);
+	       System.out.println("enter a num");
+	       int num = sc.nextInt();
+	        int squar = num*num;
+	        System.out.println("the square for a given number is ;"+squar);
+	}
+}
