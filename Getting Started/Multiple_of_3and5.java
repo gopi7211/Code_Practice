71e9083a07a5707a1c18a453bca6dcac087b9c07
GOPALA KRISHNA J <79375218+gopi7211@users.noreply.github.com>
2023-10-12 18:58:43 +0530

    Multiple of 3 and 5

---
+++ Getting Started/Multiple_of_3and5.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		int num = 60;
+		for( int i=1; i < num ;i++){
+		    if(i %3==0 || i%5==0 ){
+		        System.out.println(i + " ");
+		        }
+		    }
+		
+	}
+}
+
+// Multiple of 3 and 5 
